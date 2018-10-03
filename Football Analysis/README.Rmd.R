e2dc9a53dd634eddeea84c09595000eccdf2d68f
John Pessoa <john@expapp.com>
2018-10-03 17:08:25 -0400

    R commit with Markdown scripts

---
+++ Football Analysis/README.Rmd.R
@@ -0,0 +1,13 @@
+# Everything Football 
+
+This page is dedicated to data analysis for everything related to football :soccer: .
+
+### Questions Explored:
+
+- Is the Premier League the most competitive league in the world? 
+
+### Datasets:
+- [Fivethirtyeight](https://github.com/fivethirtyeight/data/tree/master/soccer-spi)
+- `spi_matches` contains match by match SPI ratings, forecasts, and outcomes starting in 2016
+- `spi_global_rankings` contains current SPI ratings for club teams
+- `spi_global_rankings_intl` contains current SPI ratings for international teams
